41c7ab051d13b227fdef0253bc02535bc7b5d7f5
Sudoer <sudoer.zero@gmail.com>
2023-12-13 10:43:34 +0300

    Add start the game button.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,14 +4,21 @@
       <h1 class="text-4xl font-bold">Tic Tac Toe</h1>
       <p class="text-gray-600 text-sm mb-4">The Multiplayer Mode</p>
       <router-link :to="{ name: 'game', params: { session: session } }">
-        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">Start The Game</button>
+        <button
+          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+        >
+          Start The Game
+        </button>
       </router-link>
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref } from "vue";
 
-const session = ref(Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15))
+const session = ref(
+  Math.random().toString(36).substring(2, 15) +
+    Math.random().toString(36).substring(2, 15)
+);
 </script>
